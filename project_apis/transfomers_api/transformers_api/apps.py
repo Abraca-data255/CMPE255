b3e96802f9c5015f9da822a065bb84721e5d1fa7
Aryan Jadon <aryan.jadon@sjsu.edu>
2021-12-10 21:06:38 -0800

    added transformers api code

--- project_apis/transfomers_api/transformers_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TransfomersApiConfig(AppConfig):
-    name = 'transformers_api'
--- project_apis/transfomers_api/transformers_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AnalysisAPI
-
-urlpatterns = [
-   path('', AnalysisAPI.as_view(), name="api_views"),
-]
